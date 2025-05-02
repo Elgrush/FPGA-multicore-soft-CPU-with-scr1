3868f994cbf9123370f072ad52343683ac05bf2b
Elgrush <nikikarp@mail.ru>
2025-05-02 14:40:06 +0300

    Initial libraries setup
    Setting up required libraries to begin the job

---
+++ mesh_3x3/inc/queue.svh
@@ -0,0 +1,9 @@
+`ifndef _queue_parameters_svh_
+`define _queue_parameters_svh_
+
+`include "mesh_3x3/inc/noc.svh"
+
+`define EN 64 // number of queue entries
+`define EN_B 6 // number of bits required to encode queue entries
+
+`endif
