5ad1cd69dbd22ba2b441ffa0103dcc5b78f7e041
Elgrush <nikikarp@mail.ru>
2025-09-14 18:05:32 +0300

    AXI interface added

--- core_addons/src/AXI_Compliant_RAM.sv
+++ core_addons/src/AXI_Compliant_RAM.sv
@@ -1,108 +1,158 @@
 module AXI_Compliant_RAM
-#(parameter RDATA_WIDTH=8, parameter RADDR_WIDTH=6)
+#(parameter RDATA_WIDTH=8, parameter RADDR_WIDTH=6, parameter CONTROL_WIDTH=2)
 (
-	input clk,
+	input clk, rst_n,
 
-	// AXI slave interface
-    // Channel AR
-	input  logic ar_addr_i,
-	input  logic ar_valid_i,
-	output logic ar_ready_o,
-
-    // Channel R
-    output logic r_data_o,
-	output logic r_valid_o,
-	input  logic r_ready_i,
-	output logic r_last_o,
-    output logic r_resp_o,
-
-    // Channel AW
-	input  logic aw_addr_i,
-	input  logic aw_valid_i,
-	output logic aw_ready_o
-
-	// Channel W
-	input  logic w_data_i,
-	input  logic w_valid_i,
-	output logic w_ready_o,
-	input  logic w_last_i,
-
-	// Channel B
-	output logic b_rep_o,
-	output logic b_val_o,
-	input  logic b_ready_i
+    axi_if.s
 
 );
 
 	// Объявление памяти
 	logic [RDATA_WIDTH-1:0] ram[2**RADDR_WIDTH-1:0];
-    logic [RADDR_WIDTH-1:0] ram_address_w, ram_address_r;
-    logic [RDATA_WIDTH-1:0] ram_data_w, ran_data_r;
-    logic [$clog2(RDATA_WIDTH)-1:0] record_counter_w, record_counter_r;
 
-	enum {AXI_FSM_IDLE_W, AXI_FSM_ADDRESS_W, AXI_FSM_MEMORY_W, AXI_FSM_WRITE_RESPONSE_W} 
-    axi_fsm_state_w, axi_fsm_state_w_next;
+    logic [RADDR_WIDTH + CONTROL_WIDTH - 1:0] ar_buffer, aw_buffer;
+    logic [$clog2(RADDR_WIDTH + CONTROL_WIDTH)-1:0] aw_counter, ar_counter;
 
-	enum {AXI_FSM_IDLE_R, AXI_FSM_ADDRESS_R, AXI_FSM_MEMORY_R} 
-    axi_fsm_state_r, axi_fsm_state_r_next; 
+    localparam[$clog2(RADDR_WIDTH + CONTROL_WIDTH)-1:0] ADDRESS_COUNTER_FULL_VALUE = RADDR_WIDTH+CONTROL_WIDTH-1;
+
+    logic [RDATA_WIDTH - 1:0] r_buffer, w_buffer;
+    logic [$clog2(RDATA_WIDTH) - 1:0] r_counter, w_counter;
+
+    logic w_last;
+
+    localparam[$clog2(RDATA_WIDTH)-1:0] DATA_COUNTER_FULL_VALUE = RDATA_WIDTH-1;
+
+    enum { INPUTTING, FULL } ar_state, ar_state_next;
+    enum { INPUTTING, FULL } aw_state, aw_state_next;
+
+    enum { INPUTTING, FULL } r_state, r_state_next;
+    enum { INPUTTING, FULL } w_state, w_state_next;
+
+    enum { INPUTTING, FULL } b_state, b_state_next;
 
     always_ff @( posedge clk or negedge rst_n ) begin : StateSwitchBlock
-        if(!rst_n) begin
-            axi_fsm_state_w <= AXI_FSM_IDLE_W;
+        if(!rst_n) begin            
+            ar_state <= INPUTTING;
+            ar_state_next <= INPUTTING;
+ 
+            aw_state <= INPUTTING;
+            aw_state_next <= INPUTTING;
+ 
+            r_state <= INPUTTING;
+            r_state_next <= INPUTTING;
+
+            w_state <= INPUTTING;
+            w_state_next <= INPUTTING;
+
+            b_state <= INPUTTING;
+            b_state_next <= INPUTTING;
         end else begin
-            axi_fsm_state_w <= axi_fsm_state_w_next;
+            ar_state <= ar_state_next;
+            aw_state <= aw_state_next;
+            r_state <= r_state_next;
+            w_state <= w_state_next;
+            b_state <= b_state_next;
         end
     end : StateSwitchBlock
 
-    always_ff @( posedge clk or negedge rst_n ) begin : NextStateBlock
-        if(!rst_n) begin
-            axi_fsm_state_w_next <= AXI_FSM_IDLE_W;
-            b_val_o <= 1'b0;
-        end else begin
-            axi_fsm_state_w_next <= axi_fsm_state_w;
-            case (axi_fsm_state_w)
-                AXI_FSM_ADDRESS_W : 
-                    if(ar_valid_i) begin
-                        ram_address_w[record_counter_w] <= ar_addr_i;
-                        record_counter_w <= record_counter_w + 1'b1;
-                        if(record_counter_w + 1'b1 == RADDR_WIDTH) begin
-                            record_counter_w <= '0;
-                            axi_fsm_state_w_next <= AXI_FSM_MEMORY_W;
-                        end
-                    end
-                AXI_FSM_MEMORY_W :
-                    if(w_valid_i) begin
-                        ram_data_w[record_counter_w] <= w_data_i;
-                        record_counter_w <= record_counter_w + 1'b1;
-                        if(record_counter_w + 1'b1 == RDATA_WIDTH) begin
-                            record_counter_w <= '0;
-                            ram[ram_address_w] <= ram_data_w;
-                            if(w_last_i) begin
-                                axi_fsm_state_w_next <= AXI_FSM_WRITE_RESPONSE_W;
-                            end
-                        end
-                    end
-                AXI_FSM_WRITE_RESPONSE_W :
-                    if(b_ready_i) begin
-                        // TODO read synrtacore type response
-                    end
-                default: // AKA IDLE state
-                    ram_address_w[0] <= ar_addr_i;
-                    record_counter_w <= 1'b1;
-                    if(ar_valid_i) axi_fsm_state_w_next <= AXI_FSM_ADDRESS_W;
-            endcase
-        end
-    end : NextStateBlock
-
     always_comb begin : FSMOutputBlock
-        ar_ready_o = 1'b0;
-        w_ready_o  = 1'b0;
-        case (axi_fsm_state_w)
-            AXI_FSM_MEMORY_W : w_ready_o = 1'b1;
-            AXI_FSM_WRITE_RESPONSE_W: b_val_o = 1'b1;
-            default: // AKA IDLE state with ADDRESS state
+        case (aw_state)
+            FULL:
+                aw_ready_o = 1'b0;
+            default:
+                aw_ready_o = 1'b1;
+        endcase : aw_state
+
+        case (w_state)
+            FULL:
+                w_ready_o = 1'b0;
+            default:
+                w_ready_o = 1'b1;
+        endcase : w_state
+
+        case (b_state)
+            FULL:
+                b_ready_o = 1'b0;
+            default:
+                b_ready_o = 1'b1;
+        endcase : b_state
+
+        case (ar_state)
+            FULL:
+                ar_ready_o = 1'b0;
+            default:
                 ar_ready_o = 1'b1;
-        endcase
+        endcase : ar_state
+
+        case (r_state)
+            FULL:
+                r_ready_o = 1'b0;
+            default:
+                r_ready_o = 1'b1;
+        endcase : r_state
     end : FSMOutputBlock
+
+    always_ff @( posedge clk or negedge rst_n ) begin : LogicBlock
+        if(!rst_n) begin            
+            ar_buffer  <= '0;
+            ar_counter <= '0;
+
+            aw_state   <= '0;
+            aw_counter <= '0;
+            
+            r_state    <= '0;
+            r_counter  <= '0;
+            
+            w_state    <= '0;
+            w_counter  <= '0;
+            w_last     <= '0;
+            
+            b_state    <= '0;
+            b_counter  <= '0;
+        end else begin
+            case (aw_state)
+                INPUTTING:
+                    if(aw_valid_i) begin
+                        aw_buffer[aw_counter] <= aw_addr_i;
+                        aw_counter <= aw_counter + 1'b1;
+                        if(aw_counter == ADDRESS_COUNTER_FULL_VALUE) begin
+                            aw_state_next <= FULL;
+                            aw_counter <= '0;
+                        end
+                    end
+                default: // FULL
+            endcase : aw_state
+
+            case (ar_state)
+                INPUTTING:
+                    if(ar_valid_i) begin
+                        ar_buffer[ar_counter] <= ar_addr_i;
+                        ar_counter <= ar_counter + 1'b1;
+                        if(ar_counter == ADDRESS_COUNTER_FULL_VALUE) begin
+                            ar_state_next <= FULL;
+                            ar_counter <= '0;
+                        end
+                    end
+                default: // FULL
+            endcase : ar_state
+
+            case (w_state)
+                INPUTTING:
+                    if(w_valid_i) begin
+                        w_buffer[ar_counter] <= w_data_i;
+                        w_counter <= w_counter + 1'b1;
+                        if(w_counter == DATA_COUNTER_FULL_VALUE) begin
+                            w_state_next <= FULL;
+                            w_counter <= '0;
+                        end
+                    end
+                default: // FULL
+                    if(aw_state == FULL) begin
+                        //
+                    end
+            endcase : w_state
+
+        end
+    end : LogicBlock
 
 endmodule : AXI_Compliant_RAM
